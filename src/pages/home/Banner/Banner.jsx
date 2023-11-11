e08a60c678924481206ccbf432fd14b701f68950
mahfuja5768 <mahfujaahsanmuna@gmail.com>
2023-11-11 16:01:51 +0600

    first commit

---
+++ src/pages/home/Banner/Banner.jsx
@@ -0,0 +1,29 @@
+
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import img1 from '../../../assets/home/banner/ban1.jpg'
+import img2 from '../../../assets/home/banner/ban2.jpg'
+import img3 from '../../../assets/home/banner/ban3.jpg'
+import img4 from '../../../assets/home/banner/ban4.jpg'
+const Banner = () => {
+    return (
+        <div className="max-w-screen-xl mx-auto">
+            <Carousel autoPlay>
+                <div>
+                    <img src={img1} />
+                </div>
+                <div>
+                    <img src={img2} />
+                </div>
+                <div>
+                    <img src={img3} />
+                </div>
+                <div>
+                    <img src={img4} />
+                </div>
+            </Carousel>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/routes/router.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+    createBrowserRouter
+  } from "react-router-dom";
+import MainLayout from '../layouts/MainLayout';
+import Home from '../pages/home/Home/Home';
+  
+
+export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout></MainLayout>,
+      children: [
+        {
+          path: "/",
+          element: <Home></Home>
+        },
+      ],
+    },
+  ]);
